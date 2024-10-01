49a8323c3c74fd7caf390d9106056cdb516f4258
Kevin Clark <kevin51606916@edu.pa.senac.br>
2024-09-30 21:38:02 -0300

    criação produto 2

--- produto/urls.py
+++ produto/urls.py
@@ -4,11 +4,10 @@ from django.urls import path
 from produto import views
 
 urlpatterns = [
-    path('', views.listar_cartao, name="listar_cartao"),
+    path('', views.frontend, name="frontend"),  # Mudei aqui para chamar a view 'frontend'
     path('formulario_post/', views.formulario_post, name="formulario_post"),
     path('deletar/<int:id>/', views.deletar_cartao, name="deletar_cartao"),
     path('atualizar/<int:id>/', views.atualizar_cartao, name="atualizar_cartao"),
     path('pagina2/', views.pagina2, name="clark"),
     path('formulario/', views.formulario, name="formulario"),
-    path('frontend/', views.frontend, name="frontend"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
