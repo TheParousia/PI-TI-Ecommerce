80d27f23cf734e1930ee3dfcad2998d70fb27e0b
Avestruz0 <nickolas51608456@edu.pa.senac.br>
2024-10-10 21:24:07 -0300

    Foi feita a aprimoração do código e feito o ajsute do botão atualizar, salvando as informações modificadas. O único problema é a iamgem não aparecer na lista de produtos
    Junto ao Monteiro e Eduardo
    
    Co-authored-by: Carlos Eduardo <CaduMlkDoido@users.noreply.github.com>

--- produto/urls.py
+++ produto/urls.py
@@ -4,7 +4,7 @@ from produto.views import atualizar_produtos, excluir_produtos, pagina_adm
 
 urlpatterns = [
     path('', pagina_adm, name='pagina_adm2'),
-    path('produtos/listar', pagina_adm, name='pagina_adm'),
+    path('produtos/listar/', pagina_adm, name='pagina_adm'),
     path('produtos/atualizar/<int:produto_id>/', atualizar_produtos, name='atualizar_produto'),
-    path('produtos/deletar/<int:produto_id>/', excluir_produtos, name='deletar_produto'),
+    path('produtos/excluir/<int:produto_id>/', excluir_produtos, name='excluir_produto'),
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Produto  # Certifique-se de ter o modelo Produto
+from .models import Produto
 
 def pagina_home(request):
     return render(request, 'pagina_home.html')
 
 def pagina_adm(request):
-    produtos = Produto.objects.all()  # Recupera todos os produtos do banco de dados
+    produtos = Produto.objects.all()
     return render(request, 'pagina_adm.html', {'produtos': produtos})
 
 def excluir_produtos(request, produto_id):
     produto = get_object_or_404(Produto, id=produto_id)
     if request.method == 'POST':
         produto.delete()
-        return redirect('pagina_adm')  # Redireciona para a lista de produtos
-    return render(request, 'excluir_produtos.html', {'produto': produto})  # Exibir confirmação
+        return redirect('pagina_adm')
+    return render(request, 'excluir_produtos.html', {'produto': produto})
 
 def atualizar_produtos(request, produto_id):
     produto = get_object_or_404(Produto, id=produto_id)
     if request.method == 'POST':
-        produto.nome = request.POST['nome']
-        produto.preco = request.POST['preco']
-        produto.imagem = request.POST['imagem']
-        produto.save()  # Salvar as alterações no banco de dados
-        return redirect('pagina_adm')  # Redireciona para a lista de produtos
+        produto.descricao = request.POST.get('descricao', produto.descricao)
+        produto.preco = request.POST.get('preco', produto.preco)
+        produto.imagem = request.POST.get('imagem', produto.imagem)
+        produto.save()
+        return redirect('pagina_adm')
     return render(request, 'atualizar_produtos.html', {'produto': produto})
