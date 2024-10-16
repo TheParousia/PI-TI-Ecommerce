296717ab49c71eb9791f019140fcb4dca89e628f
José Thiago <165227131+Pantoja25@users.noreply.github.com>
2024-10-15 21:28:16 -0300

    Implementamos a configuração de diretórios de mídia para garantir que todos os arquivos sejam armazenados e acessados de maneira eficiente e organizada. E funções Javascript.

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'files'
 STATIC_URL = 'static/'
+
+
+# Adicione as configurações da pastas para upload
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- produto/models.py
+++ produto/models.py
@@ -19,17 +19,20 @@ class Cor(models.Model):
         return self.nome
 
 class Produto(models.Model):
-    modelo = models.CharField(max_length=30)
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE)
     descricao = models.CharField(max_length=100)
     capacidade1 = models.CharField(max_length=10)
     capacidade2 = models.CharField(max_length=10)
     capacidade3 = models.CharField(max_length=10)
     qtd_estoque = models.IntegerField(default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    cor = models.CharField(max_length=30, default='')
-    marca = models.CharField(max_length=30, default='')
-    imagem = models.ImageField(upload_to='produto', default='img')
-    acessos = models.IntegerField(default=0)  # Adiciona o campo de acessos
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    imagem1 = models.ImageField(upload_to='produto', default='img/default.jpg')
+    imagem2 = models.ImageField(upload_to='produto', default='img/default.jpg')
+    imagem3 = models.ImageField(upload_to='produto', default='img/default.jpg')
+    imagem4 = models.ImageField(upload_to='produto', default='img/default.jpg')
+    acessos = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.marca} {self.modelo} ({self.cor}) - R${self.preco}"
