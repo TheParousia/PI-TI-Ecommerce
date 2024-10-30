542d8569c5b110d0caaa2c871918ef2c9f7c4806
Gabriel Pimentel <gabriel11119477@edu.pa.senac.br>
2024-10-30 18:53:06 -0300

    Ajuste de unicidade no uso das views

--- produto/urls.py
+++ produto/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from produto.views import pagina_home, views
+from . import views
 
 urlpatterns = [
-    path('', pagina_home, name='pagina_home'),    
+    path('', views.pagina_home, name='pagina_home'),
     path('produtos/detalhes/<int:produto_id>',
          views.detalhesProduto, name="detalhesProdutos"),
     path('produtos/criar/', views.tela_produto,
