1c2d9a2ed1b209871df5f57f95a168ff45bf98ff
Gabriel Pimentel <gabriel11119477@edu.pa.senac.br>
2024-10-30 18:25:18 -0300

    Remoção do bootstrap instalado via pip para usar o própio arquivo servidor na pastas de static

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'produto',
-    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/') ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
