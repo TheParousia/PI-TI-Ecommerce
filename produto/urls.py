e019785faabeb004beec67a1f8c69c906a4ac03a
Gabriel Pimentel <gabriel11119477@edu.pa.senac.br>
2024-10-30 18:25:36 -0300

    Reorganização ds rotas

--- produto/urls.py
+++ produto/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from produto import views
 
 urlpatterns = [
-  path('', views.tela_produto, name="tela_produto"),  # Tela de produto   
-  path('produtos/detalhes/<int:produto_id>', views.detalhesProduto, name="detalhesProdutos"),
-  path('produtos/criar/', views.tela_produto, name="tela_produto"),  # Tela de produto
-  path('add_infor', views.add_infor, name="add_infor"),
-  path('admin/', admin.site.urls),
+    path('', views.tela_produto, name="tela_produto"),  # Tela de produto
+    path('produtos/detalhes/<int:produto_id>',
+         views.detalhesProduto, name="detalhesProdutos"),
+    path('produtos/criar/', views.tela_produto,
+         name="tela_produto"),  # Tela de produto
+    path('add_infor', views.add_infor, name="add_infor"),
 ]
